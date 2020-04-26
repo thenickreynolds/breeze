66a30b3f010b7e252c36326e8be08d10a5770cfc
Nick Reynolds <me@nickreynolds.net>
2020-04-26 11:09:31 -0700

    Draggable proof of concept
    Added draggable and classnames libraries

--- tab-manager/src/windowscontainer/window/index.tsx
+++ tab-manager/src/windowscontainer/window/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles.css';
 import Tab from './tab';
 import { WindowInfo, TabInfo } from '../../types/Types';
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const numColors = 5;
 
@@ -26,16 +27,31 @@ class Window extends React.Component<WindowProps, WindowState> {
           <div className="window_title">Window { this.props.window.id }</div>
           <div className="tab_close" onClick={() => this.closeWindow(this.props.window.id)} />
         </div>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="tabs">
+            {(provided, snapshot) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}
+                // style={getListStyle(snapshot.isDraggingOver)}
+                >
+                { this.props.window.tabs.map((tab, index) =>
+                  <Tab tab={tab} index={index} />
+                )}
 
-        { this.props.window.tabs.map((tab: TabInfo) =>
-          <Tab tab={tab} />
-        )}
+                {provided.placeholder}
+              </div>
+            )}            
+          </Droppable>
+        </DragDropContext>
       </div>
     )
   }
 
   closeWindow(windowId : number) {
     chrome.windows.remove(windowId);
+  }
+
+  onDragEnd() {
+    // TODO
   }
 }
 
--- tab-manager/src/windowscontainer/window/tab/index.tsx
+++ tab-manager/src/windowscontainer/window/tab/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import './styles.css';
 import defaultFavicon from './default_favicon.png';
 import { TabInfo } from '../../../types/Types';
+import { Draggable } from "react-beautiful-dnd";
+import classNames from "classnames";
 
 type TabProps = {
-  tab: TabInfo
+  tab: TabInfo,
+  index: number,
 };
 
 class Tab extends React.Component<TabProps> {
@@ -16,12 +19,31 @@ class Tab extends React.Component<TabProps> {
     let tab = this.props.tab;
     let favIconUrl = tab.favIconUrl || defaultFavicon;
 
+    
+
     return (
-      <div className="item tab" onDoubleClick={() => this.activateTab(tab.id!, tab.windowId!)}>
-        <img className="tab_favicon" alt="" src={favIconUrl} />
-        <div className="tab_title">{ tab.title }</div>
-        <div className="tab_close" onClick={() => this.closeTab(tab.id!)}></div>
-      </div>
+      <Draggable key={tab.id} draggableId={tab.id.toString()} index={this.props.index}>
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            <div
+              className={classNames({
+                item: true,
+                tab: true,
+                dragging: snapshot.isDragging,
+              })}
+              onDoubleClick={() => this.activateTab(tab.id!, tab.windowId!)}
+            >
+              <img className="tab_favicon" alt="" src={favIconUrl} />
+              <div className="tab_title">{ tab.title }</div>
+              <div className="tab_close" onClick={() => this.closeTab(tab.id!)}></div>
+            </div>
+          </div>
+        )}
+        </Draggable>
     );
   }
 
